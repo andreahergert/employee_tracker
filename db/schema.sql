f8e4cb9ce56c9220489f133d3ee556ff22722e33
Andrea Hergert <ahergert24@hotmail.com>
2022-11-01 14:47:18 -0700

    added database and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,34 @@
--- schema
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES (manager_id)
+);
