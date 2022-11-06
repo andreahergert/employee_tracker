73067e51342a94cb56a45b9a73712601ed5a8a0f
Andrea Hergert <ahergert24@hotmail.com>
2022-11-06 10:50:29 -0800

    added question prompts to functions

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-// dependencies
+// Dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 
-// Connect to database, passing two parameters
+// Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        // MySQL username,
         user: 'root',
-        // MySQL password actually put that in there
         password: 'Washington24!',
         database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
 );
 
+// Starting Question
 function startingQuestion() {
     inquirer
         .prompt([
@@ -25,15 +24,15 @@ function startingQuestion() {
                 type: 'list',
                 name: 'startinglist',
                 message: 'What would you like to do?',
-                choices:    [   'View All Employees',
-                            'Add Employee',
-                            'Update Employee Role',
-                            'View All Roles',
-                            'Add Role',
-                            'View All Departments',
-                            'Add Department',
-                            'Quit'
-                            ],
+                choices: ['View All Employees',
+                    'Add Employee',
+                    'Update Employee Role',
+                    'View All Roles',
+                    'Add Role',
+                    'View All Departments',
+                    'Add Department',
+                    'Quit'
+                ],
             },
         ])
         .then(function (answers) {
@@ -53,72 +52,129 @@ function startingQuestion() {
                 addDepartment();
             } else if (answers.startinglist === 'Quit') {
                 console.log('Good-Bye')
-            } 
+            }
         })
 };
 
 // Viewing
 function viewEmployees() {
     // View All Employees
-        // Shows employee table from employees_db
-
+    // Shows employee table from employees_db
+    // undefined
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.log(results);
+        startingQuestion()
+    });
 };
 
 function viewRoles() {
     // View All Roles
-        // Shows role table from employees_db
-
+    // Shows role table from employees_db
+    // undefined
+    db.query('SELECT * FROM role', function (err, results) {
+        console.log(results);
+        startingQuestion()
+    });
 };
 
 function viewDepartments() {
     // View All Departments
-        // Shows department table from employees_db 
-
+    // Shows department table from employees_db
+    // undefined
+    db.query('SELECT * FROM department', function (err, results) {
+        console.log(results);
+        startingQuestion()
+    });
 };
 
 // Adding
-function addEmployee() {
-    // Add Employee
-        // "What is the employee's first name?"
-            // example: Sam
-        // "What is the employee's last name?"
-            // example: Kash
-        // "What is the employee's role?" [this is a list with choices]
-            // example: Sales Lead, Salesperson, Lead Engineer, Software Engineer, Account Manager, Accountant, Legal Team Lead, Lawyer, Customer Service
-        // "Who is the employee's manager?" [this is a list with choices]
-            // example: None, John Doe, Mike Chan, Ashley Rodriguez, Kevin Tupik, Kunal Singh, Malia Brown, Sarah Lourd, Tom Allen
-            // then goes back to "What would you like to do?" question
-
+function addEmployee(employeeAnswers) {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: "What is the employee's first name?",
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: "What is the employee's last name?",
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role?",
+                choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer'],
+                // Customer Service or any other role needs to be added
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: "Who is the employee's manager?",
+                choices: ['None', 'John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malia Brown', 'Sarah Lourd', 'Tom Allen'],
+                // Added employees need to show up on this list
+            },
+        ])
+        .then(function (answers) {
+            console.log("Employee added!")
+            startingQuestion()
+        })
 };
 
-function addRole() {
-    // Add Role
-        // "What is the name of the role?"
-            // example: Customer Service
-        // "What is the salary of the role?"
-            // example: 80000
-        // "Which Department does the role belong to?" [this is a list with choices]
-            // Engineering, Finance, Legal, Sales, Service
-            // then goes back to "What would you like to do?" question
-
+function addRole(roleAnswers) {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'role',
+                message: "What is the name of the role?",
+                // example: Customer Service
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: "What is the salary of the role?",
+                // example: 80000
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: "Which Department does the role belong to?",
+                choices: ['Engineering', 'Finance', 'Legal', 'Sales'],
+                // Service or any other department needs to be added
+            },
+        ])
+        .then(function (answers) {
+            console.log("Role added!")
+            startingQuestion()
+        })
 };
 
-function addDepartment() {
-    // Add Department
-        // "What is the name of the department?"
-            // example: Service then "Added Service to the database"
-        // then goes back to "What would you like to do?" question
-
+function addDepartment(departmentAnswers) {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: "What is the name of the department?",
+                // example: Service 
+            },
+        ])
+        .then(function (answers) {
+            console.log("Added " + answers.department + " to the database!")
+            startingQuestion()
+        })
 };
 
 // Updating
 function updateRole() {
     // Update Employee Role
-        // "Which employee's role do you want to update?" [this is a list with choices]
-            // example: John Doe, Mike Chan, Ashley Rodriguez, Kevin Tupik, Kunal Singh, Malia Brown, Sarah Lourd, Tom Allen [Sam Cash or new employee needs to be listed]
-        // "Which role do you want to assign the selected employee?" [this is a list with choices]
-            // example: Sales Lead, Salesperson, Lead Engineer, Software Engineer, Account Manager, Accountant, Legal Team Lead, Lawyer, Customer Service
-            // then goes back to "What would you like to do?" question
+    // "Which employee's role do you want to update?" [this is a list with choices]
+    // example: John Doe, Mike Chan, Ashley Rodriguez, Kevin Tupik, Kunal Singh, Malia Brown, Sarah Lourd, Tom Allen [Sam Cash or new employee needs to be listed]
+    // "Which role do you want to assign the selected employee?" [this is a list with choices]
+    // example: Sales Lead, Salesperson, Lead Engineer, Software Engineer, Account Manager, Accountant, Legal Team Lead, Lawyer, Customer Service
+    // then goes back to "What would you like to do?" question
 
 };
 
